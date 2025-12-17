501776b2c42ba12060b0b306a1e27b5754236121
Yoga Gunawan <ycgun32@gmail.com>
2025-12-17 00:46:31 -0500

    working on particles

--- Assets/Scripts/Projectiles/JCUSlamDamage.cs
+++ Assets/Scripts/Projectiles/JCUSlamDamage.cs
@@ -4,10 +4,18 @@ public class JCUSlamDamage : MonoBehaviour
 {
     public float damage;
     public float knockback;
+    public GameObject particle;
 
     private void Start()
     {
-        Invoke("DestroySelf", 0.25f);
+        Invoke("DestroySelf", 0.5f);
+        if (particle == null)
+        {
+            Debug.Log("no particle");
+            return;
+        }
+        GameObject part = Instantiate(particle, transform.position + new Vector3(0.0f, -0.15f, 0.0f), Quaternion.identity);
+        part.transform.localScale = new Vector3(0.08f, 0.08f, 0.08f);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
